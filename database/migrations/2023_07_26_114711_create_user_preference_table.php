ab28382d32f47753e245426a28802dd7ba2eb4a4
sadam3748 <sadam3748@gmail.com>
2023-07-28 10:14:21 +0500

    add sort id

--- database/migrations/2023_07_26_114711_create_user_preference_table.php
+++ database/migrations/2023_07_26_114711_create_user_preference_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('user_preferences', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('sort_id')->nullable();
             $table->string('nursing_colleges')->nullable();
             $table->timestamps();
         });
